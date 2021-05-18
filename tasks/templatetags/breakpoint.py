63dd7eda3b40bf6e70cc1ba97d8030980db4291c
Nicholas Moen <arcanemachine@tutanota.com>
2021-05-18 13:52:06 -0600

    Allow passing of custom variables as args

--- tasks/templatetags/breakpoint.py
+++ tasks/templatetags/breakpoint.py
@@ -1,19 +1,28 @@
 from django import template
-
-from project_folder.server_config import SERVER_TYPE
+from django.conf import settings
 
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def set_breakpoint(context):
+def set_breakpoint(context, *args):
     """
-    Set breakpoints in the template for easy examination of the context.
+    Set breakpoints in the template for easy examination of the context,
+    or any variables of your choice.
 
     Usage:
         {% load breakpoint %}
         {% set_breakpoint %}
+              - or -
+        {% set_breakpoint your_variable your_other_variable %}
+
+    - The context is always accessible in the pdb console as a dict 'context'.
+
+    - Custom variables can be accessed as vars[i] in the pdb console.
+        - vars[0] is your_variable, vars[1] is your_other_variable
     """
 
-    if SERVER_TYPE == 'dev':
+    vars = [arg for arg in locals()['args']]  # noqa F841
+
+    if settings.DEBUG:
         breakpoint()
