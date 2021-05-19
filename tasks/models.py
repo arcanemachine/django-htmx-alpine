adbf362d5e088001d8205fc1e6d7dfa4706fd1ad
Nicholas Moen <arcanemachine@tutanota.com>
2021-05-18 22:50:36 -0600

    Only show tasks for a given user. List is more responsive, but now does a lot more work for each operation

--- tasks/models.py
+++ tasks/models.py
@@ -1,7 +1,11 @@
+from django.conf import settings
 from django.db import models
 
 
 class Task(models.Model):
+    user = \
+        models.ForeignKey(
+            settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     description = models.CharField(max_length=127, null=True)
     is_complete = models.BooleanField(default=False)
 
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup as BS  # noqa
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.views.decorators.http import require_http_methods
 from django.shortcuts import get_object_or_404, render
@@ -9,31 +10,34 @@ from project_folder import helpers as h
 
 
 def task_list(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.filter(user=request.user)
     response = render(request, 'tasks/task_list.html', {'tasks': tasks})
     return response
 
 
+@login_required
 @require_http_methods(['POST'])
 def task_create(request):
     if request.POST.get('description', None):
-        task = Task.objects.create(description=request.POST['description'])
+        task = Task.objects.create(
+            user=request.user,
+            description=request.POST['description'])
     else:
         response = HttpResponse()
         response.status_code = 400
         return response
 
-    tasks = Task.objects.all()
-    context = {
-        'task': task,
-        'task_count': tasks.count()}
+    tasks = Task.objects.filter(user=request.user)
+    context = {'tasks': tasks,
+               'task': task}
 
-    return render(request, 'tasks/get_task.html', context)
+    return render(request, 'tasks/list_task.html', context)
 
 
+@login_required
 @require_http_methods(['PUT'])
 def task_update(request, task_id):
-    task = get_object_or_404(Task, id=task_id)
+    task = get_object_or_404(Task, id=task_id, user=request.user)
 
     parsed_params = h.get_parsed_params(request)
 
@@ -60,8 +64,15 @@ def task_update(request, task_id):
     return response
 
 
+@login_required
 @require_http_methods(['DELETE'])
 def task_delete(request, task_id):
-    task = get_object_or_404(Task, id=task_id)
+    task = get_object_or_404(Task, id=task_id, user=request.user)
     task.delete()
+
+    tasks = Task.objects.filter(user=request.user)
+    context = {'tasks': tasks,
+               'task': task}
+
+    return render(request, 'tasks/list_task.html', context)
     return HttpResponse('')
