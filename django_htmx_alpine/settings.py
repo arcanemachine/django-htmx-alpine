5283891295188213ed1ba6fbfceda82d27ea6547
Nicholas Moen <arcanemachine@tutanota.com>
2021-05-26 10:58:30 -0600

    Merge settings into fewer lines

--- django_htmx_alpine/settings.py
+++ django_htmx_alpine/settings.py
@@ -7,49 +7,41 @@ DEBUG = server_config.DEBUG
 
 ALLOWED_HOSTS = ['*']
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    # local
-    'tasks.apps.TasksConfig',
-    'users.apps.UsersConfig',
-    # third-party
-    'captcha',
-    ]
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth',
+                  'django.contrib.contenttypes', 'django.contrib.sessions',
+                  'django.contrib.messages', 'django.contrib.staticfiles',
+                  # local
+                  'tasks.apps.TasksConfig',
+                  'users.apps.UsersConfig',
+                  # third-party
+                  'captcha']
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware']
+MIDDLEWARE = ['django.middleware.security.SecurityMiddleware',
+              'django.contrib.sessions.middleware.SessionMiddleware',
+              'django.middleware.common.CommonMiddleware',
+              'django.middleware.csrf.CsrfViewMiddleware',
+              'django.contrib.auth.middleware.AuthenticationMiddleware',
+              'django.contrib.messages.middleware.MessageMiddleware',
+              'django.middleware.clickjacking.XFrameOptionsMiddleware']
 
 ROOT_URLCONF = 'django_htmx_alpine.urls'
 
-TEMPLATES = [{
-    'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': ['templates'],
-    'APP_DIRS': True,
-    'OPTIONS': {
-        'context_processors': [
-            'django.template.context_processors.debug',
-            'django.template.context_processors.request',
-            'django.contrib.auth.context_processors.auth',
-            'django.contrib.messages.context_processors.messages',
-            'project_folder.context_processors.helpers']}}]
+TEMPLATES = [{'BACKEND': 'django.template.backends.django.DjangoTemplates',
+              'DIRS': ['templates'],
+              'APP_DIRS': True,
+              'OPTIONS': {
+                  'context_processors': [
+                      'django.template.context_processors.debug',
+                      'django.template.context_processors.request',
+                      'django.contrib.auth.context_processors.auth',
+                      'django.contrib.messages.context_processors.messages',
+                      'project_folder.context_processors.helpers']}}]
 
 WSGI_APPLICATION = 'django_htmx_alpine.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'}}
+DATABASES = {'default': {
+                 'ENGINE': 'django.db.backends.sqlite3',
+                 'NAME': BASE_DIR / 'db.sqlite3'}}
 
 PW_VALIDATION_PREFIX = 'django.contrib.auth.password_validation'
 AUTH_PASSWORD_VALIDATORS = [
@@ -60,9 +52,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = server_config.TIME_ZONE
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
+USE_I18N = USE_L10N = USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -77,6 +67,7 @@ CAPTCHA_LENGTH = 5
 CAPTCHA_NOISE_FUNCTIONS = ('captcha.helpers.noise_dots',)
 CAPTCHA_LETTER_ROTATION = (-15, 20)
 CAPTCHA_TIMEOUT = 3
+CAPTCHA_TEST_MODE = DEBUG  # enable captcha test mode if DEBUG == True
 
 # static
 STATIC_URL = server_config.STATIC_URL
