e95d7e3dc015338ea0e3c5a0bd50312a3c03279c
Nicholas Moen <arcanemachine@tutanota.com>
2021-05-27 12:07:01 -0600

    Update server_config.py.default

--- django_htmx_alpine/server_config.default.py
+++ django_htmx_alpine/server_config.default.py
@@ -1,1 +1,16 @@
+from os.path import join as os_path_join
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+DEBUG = True
+
+SERVER_TYPE = 'dev'
+SERVER_URL = 'http://192.168.1.120:8000'
+
+# i18n
 TIME_ZONE = 'UTC'
+
+# static
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os_path_join(BASE_DIR, 'static')]
+STATIC_ROOT = None
