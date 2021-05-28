26b70c8f997e99f10ad9086db4d28bbc5bd6a6a4
Nicholas Moen <arcanemachine@tutanota.com>
2021-05-28 10:16:17 -0600

    Create basic test for unauthenticated user attempting to create new todo item

--- testcafe/taskList.spec.js
+++ testcafe/taskList.spec.js
@@ -16,3 +16,27 @@ fixture `tasks:task_list`
 test('sanity check', async t => {
   await t.expect(logger.contains(r => r.response.statusCode === 200)).ok()
 })
+
+let newTaskMessage = 'Test task';
+
+test('unauthenticated user cannot create new task', async t => {
+  let newTaskInputText = await Selector('#new-task-input-text');
+  let todoListMessage = await Selector('#todo-list-message');
+  let statusMessageNotification = await Selector(
+    '#status-message-notification');
+
+  // element below text input warns user to login first
+  await t.expect(todoListMessage.textContent).contains("You must login");
+
+  // attempt to create new todo
+  await t.typeText(newTaskInputText, newTaskMessage);
+  await t.click('#new-task-button-create');
+
+  // does not succeed, contains status message telling user to login
+  await t.expect(statusMessageNotification.textContent)
+    .contains("You must login");
+
+  // todo list is still empty
+  await t.expect(Selector('#todo-list').exists).notOk();
+})
+
