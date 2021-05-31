758ee144a92c2355e8c6134d4ed078516933c5ef
Nicholas Moen <arcanemachine@tutanota.com>
2021-05-30 19:09:35 -0600

    Add helper function to convert response.content to readable, formatted HTML

--- django_htmx_alpine/helpers.py
+++ django_htmx_alpine/helpers.py
@@ -1,3 +1,9 @@
+def get_formatted_html(response):
+    from bs4 import BeautifulSoup
+    return BeautifulSoup(
+        response.content.decode('utf-8'), features='html.parser')
+
+
 def get_parsed_params(request):
     params = request.body.decode('utf-8')
     parsed_params = dict([item.split('=') for item in params.split('&')])
