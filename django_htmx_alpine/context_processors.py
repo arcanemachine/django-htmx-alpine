bbac074957ab9568847fe44ad2aad33873e13a26
Nicholas Moen <arcanemachine@tutanota.com>
2021-05-31 16:27:21 -0600

    Remove unnecessary reference to 'settings' from context processor
    It's already included, so there's no need to include it manually

--- django_htmx_alpine/context_processors.py
+++ django_htmx_alpine/context_processors.py
@@ -1,7 +1,5 @@
-from django.conf import settings
 from project_folder import server_config
 
 
 def helpers(request):
-    return {'settings': settings,
-            'server_config': server_config}
+    return {'server_config': server_config}
