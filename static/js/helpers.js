d24eacce8e06f082b106387f63dd5e22fb9fb51c
Nicholas Moen <arcanemachine@tutanota.com>
2021-06-17 10:56:33 -0600

    Add helper function for handling Tab keypress in modals

--- static/js/helpers.js
+++ static/js/helpers.js
@@ -4,3 +4,11 @@ const defaultMessageTimeout = 4000;
 const hDispatch = (eventName, params={}) => {
   return window.dispatchEvent(new CustomEvent(eventName, { detail: params }));
 }
+
+function hHandleTabEvent(e, nextElement, previousElement) {
+  if (e.shiftKey) {
+    previousElement.focus();
+  } else {
+    nextElement.focus();
+  }
+}
