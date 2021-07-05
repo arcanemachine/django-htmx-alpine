e141cc6113c8a15819376c09ecc9c29d9ba6def5
Nicholas Moen <arcanemachine@tutanota.com>
2021-07-04 23:36:04 -0600

    (minor) Remove unnecessary lines, formatting

--- manage.py
+++ manage.py
@@ -2,12 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from project_folder import settings
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_htmx_alpine.settings')
+    os.environ.setdefault(
+        'DJANGO_SETTINGS_MODULE', 'django_htmx_alpine.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
