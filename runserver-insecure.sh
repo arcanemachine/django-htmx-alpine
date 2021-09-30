ac81ef4bc7f400b4d0c35765685c9f5beba68a6a
Nicholas Moen <arcanemachine@tutanota.com>
2021-09-30 16:54:05 -0600

    Update runserver scripts

--- runserver-insecure.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-stty sane  # prevent garbled text over ssh
-$DJANGO_BASE_DIR/manage.py runserver --insecure 0.0.0.0:8000
--- scripts/runserver.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-stty sane  # prevent garbled text over ssh
-python $DJANGO_BASE_DIR/manage.py runserver 0.0.0.0:8005
